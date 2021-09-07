7d67d71ed022e04f4bb3af163445c1ab7157a4da
HPhuc <ffqphuc@gmail.com>
2021-09-07 09:21:07 +0700

    Add Entities

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 ## Technologies
 - ASP.NET 5.0
 - Entity Framework Core 5
+## Install Packages
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Tools
+- Microsoft.EntityFrameworkCore.Design
 ## How to configure and run
 ## How to contribute
---
+++ eShopSolution.Data/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Data.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Data.Entities
+{
+    class Category
+    {
+        public int Id { set; get; }
+
+        public int SortOrder { set; get; }
+
+        public bool IsShowOnHome { set; get; }
+
+        public int? ParentId { set; get; }
+
+        public Status Status { set; get; }
+    }
+}
